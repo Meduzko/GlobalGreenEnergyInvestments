5e201ef1e90048f3af137413b0c32425dadcff60
Konstantin Kramarenko <kasya4ek@gmail.com>
2015-03-03 01:57:52 +0200

    Change logo and icons on svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'), // Сообственно Gulp JS
-    sass = require('gulp-sass'), // Сообственно Gulp JS
+    sass = require('gulp-sass'),
     csso = require('gulp-csso'), // Минификация CSS
     imagemin = require('gulp-imagemin'), // Минификация изображений
+    svgo = require('gulp-svgo'),
     uglify = require('gulp-uglify'), // Минификация JS
     concat = require('gulp-concat'), // Склейка файлов
-
+    svgSprite = require("gulp-svg-sprites"),
     assetsRoot = 'app/assets/',
     publicRoot = 'public/',
     vendorRoot = 'vendor/assets/';
@@ -22,6 +23,7 @@ gulp.task('js', function () {
 gulp.task('css', function () {
     gulp.src([
         assetsRoot + 'stylesheets/reset.scss',
+        assetsRoot + 'stylesheets/general.scss',
         assetsRoot + 'stylesheets/*.scss'
     ])
         .pipe(sass())
@@ -35,9 +37,26 @@ gulp.task('css', function () {
 gulp.task('images', function () {
     gulp.src([
         assetsRoot + 'images/*/*',
+        !assetsRoot + 'images/svg/*',
         assetsRoot + 'images/*'
     ])
         .pipe(imagemin())
+        .pipe(gulp.dest(publicRoot + 'images'));
+});
+
+
+gulp.task('svg-optimization', function () {
+    gulp.src(assetsRoot + 'images/*.svg')
+        .pipe(svgo())
+        .pipe(gulp.dest(publicRoot + 'images'));
+});
+
+
+gulp.task('svg-sprites', function () {
+    return gulp.src(assetsRoot + 'images/svg/*.svg')
+        .pipe(svgSprite({
+            padding: 0
+        }))
         .pipe(gulp.dest(publicRoot + 'images'));
 });
 
@@ -53,7 +72,7 @@ gulp.task('add-vendors', function () {
 });
 
 
-gulp.task('watch', ['images', 'js', 'css', 'add-vendors'], function () {
+gulp.task('watch', ['images', 'svg-optimization', 'svg-sprites', 'js', 'css', 'add-vendors'], function () {
     gulp.watch(assetsRoot + 'javascripts/*.js', ['js']);
     gulp.watch(assetsRoot + 'images/*', ['images']);
     gulp.watch(assetsRoot + 'stylesheets/*.scss', ['css']);
