d5d26328bbf5055efb176b70c539a9f2216df998
Verbovoy Maxim <vermaxik@gmail.com>
2015-04-06 23:36:19 +0300

    Added needed pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  def show
-    @page = Page.active.find_by_slug(params[:slug])
-    if @page
-      @tabs = @page.page_tabs.active
-    else
-      redirect_to root_path and return
-    end
+  def idea
+  end
+
+  def invest
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'idea' => 'pages#idea', :as => :page_idea
+  get 'invest' => 'pages#invest', :as => :page_invest
+  get 'about' => 'pages#about', :as => :page_about
+  get 'contact' => 'pages#contact', :as => :page_contact
+
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, :controllers => { registrations: 'registrations' }
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -10,6 +15,6 @@ Rails.application.routes.draw do
     end
   end
   get 'profile' => 'profiles#edit', :as => :profile
-  get ':slug' => 'pages#show',  :as => :pages
+  #get ':slug' => 'pages#show',  :as => :pages
   #resources 'profiles', only: [:create, :edit, :update]
 end
