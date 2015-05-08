0408ed4a74b133afcaf93879026aa284b25ab4ab
Verbovoy Maxim <vermaxik@gmail.com>
2015-05-08 17:01:13 +0300

    Added constuls to js response

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,7 +5,7 @@ class PasswordsController < Devise::PasswordsController
     @user = User.send_reset_password_instructions(params[:user])
     if successfully_sent?(@user)
       render :status => 200,
-             :json => { :status => 'success', :success => 'OK' }
+             :json => { :status => 'success_pass', :success => 'OK' }
     else
       render :status => 422, :json => {:status => 'errors', :errors => @user.errors.full_messages}
     end
