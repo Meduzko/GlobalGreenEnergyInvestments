eee5ead09ed8c7d26d63a54b31d3e7962702ca0a
Konstantin Kramarenko <kasya4ek@gmail.com>
2015-05-08 17:05:37 +0300

    Small UI improvements

--- app/assets/javascripts/components/sendFormPopup.js
+++ app/assets/javascripts/components/sendFormPopup.js
@@ -3,6 +3,7 @@ define(['jquery', 'components/popup'], function ($, popup) {
     return compSupport.extend(popup, {
         init: function (domId, config) {
             this.url = config.url;
+            this.needRedirect = config.needRedirect;
             this.messageBox = '.message-box';
             popup.init.apply(this, arguments);
         },
@@ -28,14 +29,23 @@ define(['jquery', 'components/popup'], function ($, popup) {
                 })
                     .done(function (data) {
                         if (data.status === 'errors') {
-                            thisComp.showMessage(data.errors, 'errors');
+                            thisComp.showMessage(data.errors, 'error');
                         } else {
-                            thisComp.hide();
-                            window.location.href = data.redirect_to;
+                            //thisComp.hide();
+                            thisComp.showMessage(data.success, 'success');
+                            if (thisComp.needRedirect) {
+                                window.location.href = data.redirect_to;
+                            }
                         }
                     })
                     .fail(function (data) {
-                        thisComp.showMessage(data.responseText, 'errors');
+                        console.log(data);
+                        if (data.status === 422) {
+                            var parsedData = JSON.parse(data.responseText);
+                            thisComp.showMessage(parsedData.errors, 'error');
+                        } else {
+                            thisComp.showMessage(data.responseText, 'error');
+                        }
                     });
             }
         }
--- app/assets/javascripts/components/tabs.js
+++ app/assets/javascripts/components/tabs.js
@@ -2,10 +2,10 @@ define(['jquery', 'jquery.responsiveTabs'], function ($) {
 
     return {
         init: function (domId) {
-
             $('#' + domId).responsiveTabs({
                 active: 0,
                 animation: "slide",
+                setHash: true,
                 duration: 300
             }).show();
         }
