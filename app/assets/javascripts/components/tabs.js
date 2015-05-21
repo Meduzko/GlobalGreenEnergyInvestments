4577731dc340ea2d7f0c0ac576f9b4402cf47c42
Konstantin Kramarenko <kasya4ek@gmail.com>
2015-05-22 00:02:14 +0300

    Small UI fixes

--- app/assets/javascripts/components/tabs.js
+++ app/assets/javascripts/components/tabs.js
@@ -4,10 +4,13 @@ define(['jquery', 'jquery.responsiveTabs'], function ($) {
 
     return {
         init: function (domId) {
+
+            var isMobile = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase());
+
             var tabs = $('#' + domId).responsiveTabs({
                 active: 0,
                 animation: "slide",
-                scrollToAccordion: true,
+                scrollToAccordion: isMobile,
                 setHash: true,
                 rotate: false,
                 collapsible: false,
