d7fe4b18d022df1ed5f51d30a11f0bb1efffd1ae
Verbovoy Maxim <vermaxik@gmail.com>
2015-10-04 13:28:22 +0300

    Added dynamic info to payment popup

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,10 @@
 module ProfilesHelper
+
+  def short_user_info
+    user_info = ''
+    user_info += current_user.profile.date_of_birthday.to_s(:normal) + ', ' if current_user.profile.present?
+    user_info += current_user.profile.country_name + ', ' if current_user.profile.country_name.present?
+    user_info += current_user.profile.address_city if current_user.profile.address_city.present?
+    user_info
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  def country_name
+    ISO3166::Country[self.country].name if self.country.present?
+  end
 end
