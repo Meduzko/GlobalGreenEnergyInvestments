1edefce57c3d813439676a01f86453afc141c7fd
Konstantin Kramarenko <kasya4ek@gmail.com>
2015-11-16 21:33:50 +0200

    Add statistic counter component

--- app/assets/javascripts/components/statisticCounter.js
+++ app/assets/javascripts/components/statisticCounter.js
@@ -3,7 +3,11 @@ define(['jquery'], function ($) {
     'use strict';
 
     return {
+
+        DEFAULT_INTERVAL: 1000,
+
         init: function (domId, config) {
+
             this.savedStatistic = $('#' + domId + ' .savedStatistic');
             this.config = config;
             this.updateStat();
@@ -13,10 +17,11 @@ define(['jquery'], function ($) {
 
             var thisComp = this;
             var step = parseFloat(thisComp.config.stepValue).toFixed(2);
+            var interval = thisComp.config.updateInterval || thisComp.DEFAULT_INTERVAL;
 
             setInterval(function () {
                 thisComp.savedStatistic.text((parseFloat(parseFloat(thisComp.savedStatistic.text()).toFixed(2)) + parseFloat(step)).toFixed(2));
-            }, thisComp.config.updateInterval)
+            }, interval)
         }
     }
 
