bd098ac976e90b817342583aaa3684cb8062bd90
Verbovoy Maxim <vermaxik@gmail.com>
2015-12-20 19:43:01 +0200

    Fixed issue

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,10 +1,12 @@
 module ProfilesHelper
 
   def short_user_info
-    user_info = ''
-    user_info += current_user.profile.date_of_birthday.to_s(:normal) + ', ' if current_user.profile.present?
-    user_info += current_user.profile.country_name + ', ' if current_user.profile.country_name.present?
-    user_info += current_user.profile.address_city if current_user.profile.address_city.present?
-    user_info
+    if current_user && current_user.profile
+      user_info = ''
+      user_info += current_user.profile.date_of_birthday.to_s(:normal) + ', ' if current_user.profile.date_of_birthday.present?
+      user_info += current_user.profile.country_name + ', ' if current_user.profile.country_name.present?
+      user_info += current_user.profile.address_city if current_user.profile.address_city.present?
+      user_info
+    end
   end
 end
